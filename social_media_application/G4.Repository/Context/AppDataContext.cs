5f4f80ef49eb4720a32d95e2e2e4f159cffba231
Lucas Matheus <114036108+LucasMG4@users.noreply.github.com>
2024-06-22 23:10:45 -0300

    WebApplication - Entity Framework - Installing and create data context
    Installation of the Entity Framework Core library and its dependencies to be later configured to run with SQL Server.

---
+++ social_media_application/G4.Repository/Context/AppDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace G4.Infraestructure.Context {
+    public class AppDataContext : DbContext {
+
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
